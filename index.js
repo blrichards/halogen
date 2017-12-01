878e95513f036f0f2a2aa87b11b75c850bbc782a
Ben Richards <blrichards@me.com>
2017-12-01 15:00:36 -0600

    fixed index

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
+import PulseLoader from './build/PulseLoader'
+import RotateLoader from './build/RotateLoader'
+import BeatLoader from './build/BeatLoader'
+import RiseLoader from './build/RiseLoader'
+import SyncLoader from './build/SyncLoader'
+import GridLoader from './build/GridLoader'
+import ClipLoader from './build/ClipLoader'
+import SquareLoader from './build/SquareLoader'
+import DotLoader from './build/DotLoader'
+import PacmanLoader from './build/PacmanLoader'
+import MoonLoader from './build/MoonLoader'
+import RingLoader from './build/RingLoader'
+import BounceLoader from './build/BounceLoader'
+import SkewLoader from './build/SkewLoader'
+import FadeLoader from './build/FadeLoader'
+import ScaleLoader from './build/ScaleLoader'
 
-export const BeatLoader = require('./build/B')
+export default {
+  PulseLoader,
+  RotateLoader,
+  BeatLoader,
+  RiseLoader,
+  SyncLoader,
+  GridLoader,
+  ClipLoader,
+  SquareLoader,
+  DotLoader,
+  PacmanLoader,
+  MoonLoader,
+  RingLoader,
+  BounceLoader,
+  SkewLoader,
+  FadeLoader,
+  ScaleLoader,
+}
